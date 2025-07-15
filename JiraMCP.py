12d99a0a710cd8c4bb9428c19478b1eb7469312e
Isuka Premathilake <isuka.premathilake@pronto.net>
2025-07-15 12:18:45 +0530

    Changes made

--- JiraMCP.py
+++ JiraMCP.py
@@ -1,12 +1,12 @@
+import os
+
 from fastmcp import FastMCP
 
-# Give your MCP server a name
-mcp = FastMCP("MyMCPServer")
-
+mcp = FastMCP("MyMCPServer", host="localhost", port=8000)
 
 @mcp.tool()
 def greet(name: str) -> str:
     return f"Hello, {name}! Welcome to the MCP server."
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(transport="sse")
